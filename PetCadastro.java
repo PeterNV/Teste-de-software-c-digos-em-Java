0bd0a467e88726d780623cf24ddb4e8cc4cad759
PeterNV <114534711+PeterNV@users.noreply.github.com>
2024-09-11 22:46:40 -0300

    Update PetCadastro.java

--- PetCadastro.java
+++ PetCadastro.java
@@ -1,5 +1,42 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PetCadastro {
-	public Pet criarPet(String nome, String especie, int idade) {
-	return new Pet(nome, especie, idade);
-}
+	private PetArmazena repo;
+	public PetCadastro(PetArmazena repo) {
+		this.repo = repo;
+	}
+	public Pet criarPet(Pet pet) {
+		
+    	if(!this.ValidarNome(pet.getNome())) {
+    		return null;
+    	}
+    	if(!this.ValidarEspecie(pet.getEspecie())) {
+    		return null;
+    	}
+    	
+        this.repo.salvarPet(pet);
+		return pet;
+	}
+	
+	public boolean ValidarNome(String nome) {
+    	String regex = "^(?=.*[a-zA-ZÀ-ÖØ-öø-ÿ])[a-zA-ZÀ-ÖØ-öø-ÿ\\s]{3,32}$";
+        Pattern p = Pattern.compile(regex);
+        
+        Matcher m = p.matcher(nome);
+        if (m.matches()) {
+            return true;
+        }
+        return false;
+    }
+	
+	public boolean ValidarEspecie(String especie) {
+		String TodasAsEspecies[] = {"Cachorro","Gato","Papagaio","Hamster","Tartaruga"};
+        for(int i = 0; i < TodasAsEspecies.length;i++) {
+        	if(especie.equals(TodasAsEspecies[i])) {
+        		return true;
+        	}
+        }
+        return false;
+    }
 }
