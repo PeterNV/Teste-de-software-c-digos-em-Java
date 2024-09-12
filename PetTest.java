43067cc7bd758b827413a83c0fd341f17e6784f2
PeterNV <114534711+PeterNV@users.noreply.github.com>
2024-09-11 22:47:34 -0300

    Add files via upload

---
+++ PetTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
+import org.junit.Test;
+public class PetTest {
+	private PetCadastro petCadastro;
+    private PetArmazena petArmazena;
+    Pet pet1;
+    
+    @Before
+    public void init() {
+    	 petArmazena =  new PetArmazena();
+    	 petCadastro = new PetCadastro(petArmazena);
+    }
+    
+    @Test
+    public void CamposEmBranco() {
+    	assertTrue(petArmazena.getPets().isEmpty());
+    }
+    
+    @Test
+    public void testCriarPet() {
+        pet1 = new Pet("Guilbert", "Gato", 1);
+        Pet u = this.petCadastro.criarPet(pet1);
+        
+        assertNotNull(u);
+        
+    }
+    
+    @Test
+    public void testCriarPetComNomeInvalido() {
+        pet1 = new Pet("Guilbert3", "Gato", 1);
+        Pet u = this.petCadastro.criarPet(pet1);
+        
+        assertNull(u);
+        
+    }
+    
+    @Test
+    public void testCriarPetComEspecieInvalida() {
+        pet1 = new Pet("Guilbert", "Tigre", 1);
+        Pet u = this.petCadastro.criarPet(pet1);
+        
+        assertNull(u);
+        
+    }
+    
+    @Test
+    public void testSalvarUsuario() {
+        petArmazena.salvarPet(pet1);
+        assertEquals(1, petArmazena.getPets().size());
+        assertFalse(petArmazena.getPets().isEmpty());
+        
+    }
+}
