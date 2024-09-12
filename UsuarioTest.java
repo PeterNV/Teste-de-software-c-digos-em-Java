0843776d6b25493e32ced6f5cab0d89644867a18
PeterNV <114534711+PeterNV@users.noreply.github.com>
2024-09-11 22:46:20 -0300

    Update UsuarioTest.java

--- UsuarioTest.java
+++ UsuarioTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import org.junit.Before;
@@ -7,53 +8,94 @@ import org.junit.Test;
 
 public class UsuarioTest {
     private UsuarioCadastro usuarioCadastro;
-    private PetCadastro petCadastro;
+  
     private UsuarioArmazena usuarioArmazena;
-    private PetArmazena petArmazena;
+
     Usuario user1;
-    Pet pet1;
+    
     @Before
     public void init() {
-        usuarioCadastro = new UsuarioCadastro();
-        usuarioArmazena = new UsuarioArmazena();
-        petCadastro = new PetCadastro();
-        petArmazena = new PetArmazena();
-        user1 = usuarioCadastro.criarUsuario("João", "Silva Vila", "Masculino", "01/01/1990", "joao@gmail.com", 
-                "Senha123@", "Senha123@", "12345678909", "12345-678", 
-                "1122334455", "9988776655");
-        pet1 = petCadastro.criarPet("Guilbert", "Gato", 1);
+    	usuarioArmazena = new UsuarioArmazena();
+        usuarioCadastro = new UsuarioCadastro(usuarioArmazena);
     }
+    
     @Test
     public void CamposEmBranco() {
     	assertTrue(usuarioArmazena.getUsuarios().isEmpty());
     }
+    
     @Test
     public void testCriarUsuario() {
+        user1 = new Usuario("João", "Silva Vila", "Masculino", "01/01/1990", "joao@gmail.com", 
+                "Senha123@", "Senha123@", "12345678909", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
         
-        assertNotNull(user1.getNome());
-        assertNotNull(user1.getSobrenome());
-        assertNotNull(user1.getSenha());
-        assertNotNull(user1.getSexo());
-        assertNotNull(user1.getCpf());
-        assertNotNull(user1.getCelular());
-        assertNotNull(user1.getTelefone());
-        assertEquals(user1.getSenha(), user1.getConfirmaSenha());
+        assertNotNull(u);
         
+    }
+    
+    @Test
+    public void testCriarUuarioComCpfInvalido() {
+    	user1 = new Usuario("João", "Silva Vila", "Masculino", "01/01/1990", "joao@gmail.com", 
+                "Senha123@", "Senha123@", "123456789", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
+        
+        assertNull(u);
     }
     @Test
-    public void TesteCriarPet() {
-    	assertNotNull(pet1.getNome());
-        assertNotNull(pet1.getEspecie());
-        assertNotNull(pet1.getIdade());
+    public void testCriarUuarioComSenhaInvalida() {
+    	user1 = new Usuario("João", "Silva Vila", "Masculino", "01/01/1990", "joao@gmail.com", 
+                "Senha123", "Senha123@", "12345678909", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
+        
+        assertNull(u);
     }
+    @Test
+    public void testCriarUuarioComSexoInvalido() {
+    	user1 = new Usuario("João", "Silva Vila", "Não selecionado", "01/01/1990", "joao@gmail.com", 
+                "Senha123@", "Senha123@", "12345678909", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
+        
+        assertNull(u);
+    }
+    @Test
+    public void testCriarUuarioComNomeInvalido() {
+    	user1 = new Usuario("João3", "Silva Vila", "Masculino", "01/01/1990", "joao@gmail.com", 
+                "Senha123@", "Senha123@", "12345678909", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
+        
+        assertNull(u);
+    }
+    @Test
+    public void testCriarUuarioComSobrenomeInvalido() {
+    	user1 = new Usuario("João", "Silva Vil@", "Masculino", "01/01/1990", "joao@gmail.com", 
+                "Senha123@", "Senha123@", "12345678909", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
+        
+        assertNull(u);
+    }
+    @Test
+    public void testCriarUuarioComEmailInvalido() {
+    	user1 = new Usuario("João", "Silva Vila", "Masculino", "01/01/1990", "joaogmail.com", 
+                "Senha123@", "Senha123@", "12345678909", "12345-678", 
+                "1122334455", "9988776655");
+        Usuario u = this.usuarioCadastro.criarUsuario(user1);
+        
+        assertNull(u);
+    }
+    
 
     @Test
     public void testSalvarUsuario() {
         usuarioArmazena.salvarUsuario(user1);
-        petArmazena.salvarPet(pet1);
         assertEquals(1, usuarioArmazena.getUsuarios().size());
-        assertEquals(1, petArmazena.getPets().size());
         assertFalse(usuarioArmazena.getUsuarios().isEmpty());
-        assertFalse(petArmazena.getPets().isEmpty());
+        
     }
 }
