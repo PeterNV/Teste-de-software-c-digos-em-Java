1e7c44eb3a0444d5428fe2825da63d7aa4a6fd21
PeterNV <114534711+PeterNV@users.noreply.github.com>
2024-09-18 11:45:41 -0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,9 +1,23 @@
-import java.util.ArrayList;
+
 public class Login {
-	public ArrayList<String> Email(ArrayList<String> email){
-		return email;
+	
+	private Usuario userLogado;
+	private UsuarioArmazena userArmazenado;
+	public Login(UsuarioArmazena userArmazenado) {
+		this.userArmazenado = userArmazenado;
 	}
-	public ArrayList<String> Senha(ArrayList<String> senha){
-		return senha;
+	public Usuario Logar(Usuario u) {
+		Usuario user = this.ValidarUsuario(u);
+		if(user  !=  null) {
+			this.userLogado = user;
+		}
+		return this.userLogado;
+	}
+	public Usuario ValidarUsuario(Usuario u) {
+	Usuario user =	this.userArmazenado.buscarUsuario(u.getEmail());
+	if(user != null && !user.getSenha().equals(u.getSenha())) {
+		user = null;
+	}
+	return user;
 	}
 }
