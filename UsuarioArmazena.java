94a75857029158e4660764cfcc6c5aced6c1d77c
PeterNV <114534711+PeterNV@users.noreply.github.com>
2024-09-18 11:47:21 -0300

    Update UsuarioArmazena.java

--- UsuarioArmazena.java
+++ UsuarioArmazena.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 import java.util.List;
 public class UsuarioArmazena {
     private List<Usuario> usuarios;
@@ -13,5 +14,17 @@ public class UsuarioArmazena {
 
     public List<Usuario> getUsuarios() {
         return usuarios;
+    }
+    
+    public Usuario buscarUsuario(String email) {
+    	
+    	Usuario user = null;
+    	for(Usuario u: this.usuarios) {
+    		if(u.getEmail().equals(email)) {
+    			user = u;
+    			break;
+    		}
+    	}
+    	return user;
     }
 }
