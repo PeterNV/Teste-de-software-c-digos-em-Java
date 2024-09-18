047e17e59ab92ddcb6ff34bb063fd70aa961aa79
PeterNV <114534711+PeterNV@users.noreply.github.com>
2024-09-18 14:50:13 -0300

    Update TesteDeLogin.java

--- TesteDeLogin.java
+++ TesteDeLogin.java
@@ -1,19 +1,20 @@
 import org.junit.Test;
 import org.junit.Before;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-import java.util.ArrayList;
+import static org.junit.Assert.assertNull;
+
 
 public class TesteDeLogin {
 	private UsuarioCadastro usuarioCadastro;
 	 private UsuarioArmazena usuarioArmazena;
-	  @Before
-	    public void init() {
+	
+	 
+  @Before
+  public void init() {
 	    	usuarioArmazena = new UsuarioArmazena();
 	        usuarioCadastro = new UsuarioCadastro(usuarioArmazena);
-	    }
+   }
 	    
 	@Test
 	public void testeLoginSucesso() {
@@ -28,5 +29,31 @@ public class TesteDeLogin {
 		Usuario usuarioLogado = login.Logar(u);
         assertNotNull(usuarioLogado);
 	}
+	
+	@Test
+	public void testeLoginEmailInvalido() {
+	  
+	    Usuario user1 = new Usuario("João", "Silva Vila", "Masculino", "01/01/1990", "joaogmail.com",
+	            "Senha123@", "Senha123@", "12345678909", "12345-678", 
+	            "1122334455", "9988776655");
 
+	
+	    Usuario u = this.usuarioCadastro.criarUsuario(user1);
+	    assertNull(u); 
+
+	    
+	}
+	@Test
+	public void testeLoginSenhaInvalida() {
+
+	    Usuario user1 = new Usuario("João", "Silva Vila", "Masculino", "01/01/1990", "joao@gmail.com",
+	            "Senha123", "Senha123", "12345678909", "12345-678", 
+	            "1122334455", "9988776655");
+
+	    
+	    Usuario u = this.usuarioCadastro.criarUsuario(user1);
+	    assertNull(u); 
+	    
+	}
+	
 }
